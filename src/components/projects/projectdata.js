02780993edd7c9b5c7b30f7a215f284e6fa81b79
72umesh <umeshpal8141@gmail.com>
2025-03-04 15:27:05 +0530

    Added WidgetHub Project

--- src/components/projects/projectdata.js
+++ src/components/projects/projectdata.js
@@ -1,32 +1,44 @@
-import MovieList from "../../assets/MovieList.png"
-import Maharaja from "../../assets/Maharaja.png"
-import StoreX from "../../assets/StoreX.png"
+import MovieList from "../../assets/MovieList.png";
+import Maharaja from "../../assets/Maharaja.png";
+import StoreX from "../../assets/StoreX.png";
+import WidgetHub from "../../assets/WidgetHub.png";
 
 const projectData = [
-    {
-        id:1,
-        title:"MovieList",
-        img:MovieList,
-        github: "https://github.com/72umesh/MovieList/",
-        demo: "https://72umesh.github.io/MovieList/",
-        description: "A responsive React movie app with search, pagination, and a Favorites page using Hooks and an external API."
-    },
-    {
-        id:2,
-        title:"Maharaja",
-        img:Maharaja,
-        github:"https://github.com/72umesh/Maharaja-Restaurant",
-        demo: "https://72umesh.github.io/Maharaja-Restaurant/",
-        description: "Developed a visually appealing and fully responsive restaurant website, using React and styled with CSS"
-
-    },
-    {
-        id:3,
-        title:"StoreX",
-        img:StoreX,
-        github: "https://github.com/72umesh/StoreX",
-        demo: "https://72umesh.github.io/StoreX/",
-        description: "Designed a responsive Bootstrap product page with seamless navigation and dedicated sections."
-    }
-]
+  {
+    id: 1,
+    title: "MovieList",
+    img: MovieList,
+    github: "https://github.com/72umesh/MovieList/",
+    demo: "https://72umesh.github.io/MovieList/",
+    description:
+      "A responsive React movie app with search, pagination, and a Favorites page using Hooks and an external API.",
+  },
+  {
+    id: 2,
+    title: "WidgetHub",
+    img: WidgetHub,
+    github: "https://github.com/72umesh/WidgetHub",
+    demo: "https://72umesh.github.io/WidgetHub/#/",
+    description:
+      "A React-based showcase of interactive UI components and mini-projects, all in one place.",
+  },
+  {
+    id: 3,
+    title: "Maharaja",
+    img: Maharaja,
+    github: "https://github.com/72umesh/Maharaja-Restaurant",
+    demo: "https://72umesh.github.io/Maharaja-Restaurant/",
+    description:
+      "Developed a visually appealing and fully responsive restaurant website, using React and styled with CSS",
+  },
+  {
+    id: 4,
+    title: "StoreX",
+    img: StoreX,
+    github: "https://github.com/72umesh/StoreX",
+    demo: "https://72umesh.github.io/StoreX",
+    description:
+      "Designed a responsive Bootstrap product page with seamless navigation and dedicated sections.",
+  },
+];
 export default projectData;
