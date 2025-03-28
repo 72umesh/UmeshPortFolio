7ef44a3e94c9b05fb92ae490fd6e625da2f1122a
72umesh <umeshpal8141@gmail.com>
2025-03-28 17:33:26 +0530

    Added Google Analytics

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,14 @@ import About from "./components/about/About";
 import Project from "./components/projects/Project";
 import Contact from "./components/contact/Contact";
 
+import ReactGA from "react-ga4";
+import { useEffect } from "react";
+
 function App() {
+  useEffect(() => {
+    ReactGA.initialize("G-4W4BG1Q8CZ");
+    ReactGA.send("pageview");
+  }, []);
   return (
     <div>
       <Header />
