8aea95ae9a4bf28fefbacefdfea69af28203ddd5
72umesh <umeshpal8141@gmail.com>
2025-07-17 15:21:37 +0530

    Updated Tool Container

--- src/components/toolsDiv/ToolDiv.jsx
+++ src/components/toolsDiv/ToolDiv.jsx
@@ -2,14 +2,13 @@ import "./ToolDiv.css";
 
 function ToolDiv({ Icon, title, description }) {
   return (
-    <div className="tooldiv">
-      <div className="tooldiv-icon flex-center">
-        <Icon className="tool-icon" />
-      </div>
-      <div className="tooldiv-content">
+    <div className="tooldiv flex-center">
+      <Icon className="tool-icon" />
+
+      {/* <div className="tooldiv-content">
         <h3>{title}</h3>
         <p>{description}</p>
-      </div>
+      </div> */}
     </div>
   );
 }
--- src/components/toolsDiv/ToolDivData..js
+++ src/components/toolsDiv/ToolDivData..js
@@ -1,43 +1,65 @@
-import { FaHtml5,FaCss3Alt,FaJsSquare,FaReact,FaFigma,FaGithub   } from "react-icons/fa";
+import {
+  FaHtml5,
+  FaCss3Alt,
+  FaJsSquare,
+  FaReact,
+  FaFigma,
+  FaGithub,
+  FaNodeJs,
+} from "react-icons/fa";
+import { SiMysql, SiExpress } from "react-icons/si";
 
 const tools = [
-    {
-        id:1,
-        Icon: FaHtml5,
-        title:"HTML",
-        description:"The backbone of web pages, defining structure and content."
-    },
-    {
-        id:2,
-        Icon: FaCss3Alt,
-        title:"CSS",
-        description:"Transforms raw HTML into visually appealing designs."
-    },
-    {
-        id:3,
-        Icon: FaJsSquare,
-        title:"JS",
-        description:"Powers the dynamic and interactive elements of a site."
-    },
-    {
-        id:4,
-        Icon: FaReact,
-        title:"React",
-        description:"Building dynamic and reusable UI components."
-    },
-    {
-        id:5,
-        Icon: FaFigma,
-        title:"Figma",
-        description:"UX/UI Design & Prototyping."
-    },
-    {
-        id:6,
-        Icon: FaGithub,
-        title:"Github",
-        description:"Utilized for version control and hosting the code"
-    }
-    
-
+  {
+    id: 1,
+    Icon: FaHtml5,
+    title: "HTML",
+    description: "The backbone of web pages, defining structure and content.",
+  },
+  {
+    id: 2,
+    Icon: FaCss3Alt,
+    title: "CSS",
+    description: "Transforms raw HTML into visually appealing designs.",
+  },
+  {
+    id: 3,
+    Icon: FaJsSquare,
+    title: "JS",
+    description: "Powers the dynamic and interactive elements of a site.",
+  },
+  {
+    id: 4,
+    Icon: FaReact,
+    title: "React",
+    description: "Building dynamic and reusable UI components.",
+  },
+  {
+    id: 5,
+    Icon: FaFigma,
+    title: "Figma",
+    description: "UX/UI Design & Prototyping.",
+  },
+  {
+    id: 6,
+    Icon: FaGithub,
+    title: "Github",
+    description: "Utilized for version control and hosting the code",
+  },
+  {
+    id: 7,
+    Icon: FaNodeJs,
+  },
+  {
+    id: 8,
+    Icon: SiMysql,
+  },
+  {
+    id: 9,
+    Icon: SiExpress,
+  },
 ];
 export default tools;
+
+// Html, Css, JavaScript, React, Figma, Github
+// Node.js, Express, MySQl
