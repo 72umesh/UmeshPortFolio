e401dd30ab619a6ed060608cf5c4bba1558058c9
72umesh <umeshpal8141@gmail.com>
2025-09-14 19:04:39 +0530

    Fixed Healthcare Dashboard Path

--- src/components/projects/projectdata.js
+++ src/components/projects/projectdata.js
@@ -4,7 +4,7 @@ import StoreX from "../../assets/StoreX.png";
 import WidgetHub from "../../assets/WidgetHub.png";
 import Documentation from "../../assets/Documentation.png";
 import OsloDashBoard from "../../assets/Oslo_DashBoard.png";
-import HealthCareDashBoard from "../../assets/HealthCare_Dashboard.png";
+import HealthCareDashBoard from "../../assets/HealthCare_DashBoard.png";
 
 const projectData = [
   {
