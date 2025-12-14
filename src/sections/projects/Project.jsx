6bdf277c5b97004cde13e6b5ae3cc82d78303cfb
Umesh Kumar Pal <umeshpal8141@gmail.com>
2025-12-14 23:54:23 +0530

    Added fallback for empty project and updated filter options

--- src/sections/projects/Project.jsx
+++ src/sections/projects/Project.jsx
@@ -7,6 +7,13 @@ import { projectData, dashboardData } from "../../data/projectdata";
 
 function Project() {
   const [selectedCategory, setSelectedCategory] = useState("React.js");
+
+  const filteredProjects = projectData.filter((data) => {
+    if (selectedCategory === "All") return true;
+    return data.categories === selectedCategory;
+  });
+
+  console.log(filteredProjects);
   return (
     <section id="projects" className="section-padding">
       <h1 className="sectionh1">Projects</h1>
@@ -17,13 +24,8 @@ function Project() {
       />
       <div className="projects-container">
         <div className="circle-left"></div>
-        {projectData
-          .filter((data) =>
-            selectedCategory === "React.js"
-              ? data.categories === "React.js"
-              : data.categories === selectedCategory
-          )
-          .map((data) => (
+        {filteredProjects.length > 0 ? (
+          filteredProjects.map((data) => (
             <ProjectDiv
               key={data.id}
               title={data.title}
@@ -34,7 +36,16 @@ function Project() {
               github={data.github}
               isNew={data.isNew}
             />
-          ))}
+          ))
+        ) : (
+          <div className="project-empty">
+            <h3>Work In Progress</h3>
+            <p>
+              No projects in <strong>{selectedCategory}</strong> yet, <br />
+              but I'm actively working on it ,check back in 3-4 days!
+            </p>
+          </div>
+        )}
       </div>
 
       <div className="dashboard-container">
