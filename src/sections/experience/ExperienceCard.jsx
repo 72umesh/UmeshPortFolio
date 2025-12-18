78144efd2c9b0ecee1e2c181ff230a7cccc52f0b
Umesh Kumar Pal <umeshpal8141@gmail.com>
2025-12-18 17:37:29 +0530

    Made experience section responsive and added animations

--- src/sections/experience/ExperienceCard.jsx
+++ src/sections/experience/ExperienceCard.jsx
@@ -2,6 +2,7 @@ import { FaArrowUpRightFromSquare } from "react-icons/fa6";
 import { useState } from "react";
 import "./ExperienceCard.css";
 import { challenges } from "../../data/experiencedata";
+import { motion, AnimatePresence, easeInOut } from "framer-motion";
 
 function ExperienceCard({ role, duration, company, description, isView }) {
   const [isOpen, setIsOpen] = useState(false);
@@ -29,21 +30,39 @@ function ExperienceCard({ role, duration, company, description, isView }) {
               : "View My Published Questions"}
           </button>
 
-          {isOpen && (
-            <div className="challenges-container">
-              {challenges.map((c) => (
-                <a
-                  key={c.title}
-                  href={c.link}
-                  className="ch-link"
-                  target="_blank"
-                >
-                  <FaArrowUpRightFromSquare fontSize={12} />
-                  <h4 className="ch-title">{c.title}</h4>
-                </a>
-              ))}
-            </div>
-          )}
+          <AnimatePresence>
+            {isOpen && (
+              <motion.div
+                initial={{ height: 0, opacity: 0 }}
+                animate={{ height: "auto", opacity: 1 }}
+                exit={{ height: 0, opacity: 0 }}
+                transition={{
+                  duration: 0.4,
+                  ease: "easeInOut",
+                  height: { duration: 0.35, ease: "easeInOut" },
+                  scale: { duration: 0.35 },
+                  when: "beforeChildren",
+                  staggerChildren: 0.05,
+                }}
+                className="challenges-container"
+              >
+                {challenges.map((c, i) => (
+                  <motion.a
+                    key={c.title}
+                    href={c.link}
+                    className="ch-link"
+                    target="_blank"
+                    initial={{ opacity: 0, y: -10 }}
+                    animate={{ opacity: 1, y: 0 }}
+                    transition={{ delay: i * 0.05 }}
+                  >
+                    <FaArrowUpRightFromSquare fontSize={12} />
+                    <h4 className="ch-title">{c.title}</h4>
+                  </motion.a>
+                ))}
+              </motion.div>
+            )}
+          </AnimatePresence>
         </div>
       )}
     </div>
