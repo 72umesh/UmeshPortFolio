6329750443a50c321eacb3bece21d4755e4f426d
Umesh Kumar Pal <umeshpal8141@gmail.com>
2026-02-16 12:57:01 +0530

    feat/contact: Improve Contact Section with layout and design Consistency

---
+++ src/data/contactdata.js
@@ -0,0 +1,24 @@
+import { FaLinkedin, FaGithub } from "react-icons/fa";
+import { MdEmail } from "react-icons/md";
+const contacts = [
+  {
+    id: 1,
+    title: "LinkedIn",
+    Logo: FaLinkedin,
+    url: "https://www.linkedin.com/in/umeshkumarpal/",
+  },
+  {
+    id: 2,
+    title: "Github",
+    Logo: FaGithub,
+    url: "https://github.com/72umesh",
+  },
+  {
+    id: 3,
+    title: "umeshpal8141@gmail.com",
+    Logo: MdEmail,
+    url: "mailto:umeshpal8141@gmail.com",
+  },
+];
+
+export {contacts}
--- src/sections/contact/Contact.jsx
+++ src/sections/contact/Contact.jsx
@@ -1,41 +1,21 @@
 import { useState } from "react";
+import { contacts } from "../../data/contactdata";
+import { trackEvent } from "../../utils/analytics";
 import "./Contact.css";
-import { FaLinkedin, FaGithub } from "react-icons/fa";
-import { MdEmail } from "react-icons/md";
-const contacts = [
-  {
-    id: 1,
-    title: "LinkedIn",
-    Logo: FaLinkedin,
-    url: "https://www.linkedin.com/in/umeshkumarpal/",
-  },
-  {
-    id: 2,
-    title: "Github",
-    Logo: FaGithub,
-    url: "https://github.com/72umesh",
-  },
-  {
-    id: 3,
-    title: "umeshpal8141@gmail.com",
-    Logo: MdEmail,
-    url: "mailto:umeshpal8141@gmail.com",
-  },
-];
+
 function SingleContactDiv({ title, url, Logo }) {
   function handleClick() {
-    console.log("Clicked", url);
-    window.open(url, "_blank");
+    window.open(url, "_blank", "noopener,noreferrer");
+    trackEvent(`contact_${title.toLowerCase()}_click`);
   }
 
   return (
     <div
       onClick={handleClick}
-      style={{ cursor: "pointer" }}
       className="contactdiv"
     >
-      {<Logo className="logo" />}
-      <h4>{title}</h4>
+      {<Logo className="contact-link-icon" />}
+      <h4 className="contact-link-title">{title}</h4>
     </div>
   );
 }
@@ -47,98 +27,129 @@ function Contact() {
     setStatus("sending");
 
     const formData = new FormData(e.target);
-    const response = await fetch("https://formspree.io/f/xqagbyok", {
-      method: "POST",
-      body: formData,
-      headers: { Accept: "application/json" },
-    });
+    try {
+      const response = await fetch("https://formspree.io/f/xqagbyok", {
+        method: "POST",
+        body: formData,
+        headers: { Accept: "application/json" },
+      });
 
-    if (response.ok) {
-      e.target.reset();
-      setStatus("success");
-    } else {
+      if (response.ok) {
+        e.target.reset();
+        setStatus("success");
+        trackEvent("contact_form_sucess");
+        setTimeout(() => setStatus("idle"), 5000);
+      } else {
+        setStatus("error");
+        trackEvent("contact_form_error");
+      }
+    } catch (error) {
+      console.error("Error submitting form:", error);
       setStatus("error");
+      trackEvent("contact_form_error");
     }
   }
   return (
     <div id="contact" className="section-padding">
-      <h1 className="sectionh1">Get In Touch</h1>
+      <div className="container">
+        <h1 className="sectionh1">Get In Touch</h1>
 
-      <div className="main-container-contact">
-        <div className="contact-container">
-          <div className="circle-right"></div>
-
-          <p className="p-quicksand subheading">
-            Let’s turn ideas into interactive experiences. Feel free to connect,
-            collaborate, or just talk code.
-          </p>
-
-          {contacts.map((contact) => (
-            <SingleContactDiv
-              key={contact.id}
-              title={contact.title}
-              url={contact.url}
-              Logo={contact.Logo}
-            />
-          ))}
-          <p className="p-quicksand subheading">
-            Thanks for visiting — open to collaboration, internships, or
-            full-time roles!
-          </p>
-        </div>
-
-        <form className="form" onSubmit={handleSubmit}>
-          <label htmlFor="name" className="form-label p-josefin">
-            Name
-          </label>
-          <input
-            type="text"
-            name="name"
-            id="name"
-            placeholder="Your Name"
-            required
-          />
-
-          <label htmlFor="email" className="form-label p-josefin">
-            Email
-          </label>
-          <input
-            type="email"
-            name="email"
-            id="email"
-            placeholder="Your Email"
-            required
-          />
-
-          <label htmlFor="message" className="form-label p-josefin">
-            Message
-          </label>
-          <textarea
-            name="message"
-            placeholder="Your Message"
-            id="message"
-            required
-          ></textarea>
-          <button
-            type="submit"
-            disabled={status === "sending"}
-            className="p-quicksand"
-          >
-            {status === "sending"
-              ? "Sending..."
-              : status === "success"
-              ? "Sent!"
-              : "Send Message"}
-          </button>
-          {status === "success" && (
-            <p className="success-message">
-              Message sent successfully! Thank you for reaching out.
+        <div className="contact-content">
+          <div className="contact-links">
+            <h3 className="contact-content-title">Reach Out</h3>
+            <p className=" contact-link-description">
+              Let’s turn ideas into interactive experiences. Feel free to
+              connect, collaborate, or just talk code.
             </p>
-          )}
-          {status === "error" && (
-            <p className="error-message"> Something went wrong. Try again.</p>
-          )}
-        </form>
+
+            <div className="contact-links-grid">
+              {contacts.map((contact) => (
+                <SingleContactDiv
+                  key={contact.id}
+                  title={contact.title}
+                  url={contact.url}
+                  Logo={contact.Logo}
+                />
+              ))}
+            </div>
+
+            <p className="contact-availability">
+              Thanks for visiting — open to collaboration, internships, or
+              full-time roles!
+            </p>
+          </div>
+
+          <div className="contact-form-wrapper">
+            <h3 className="contact-form-title">Send a Message</h3>
+
+            <form className="contact-form" onSubmit={handleSubmit}>
+              <div className="form-group">
+                <label htmlFor="name" className="form-label">
+                  Name
+                </label>
+                <input
+                  type="text"
+                  name="name"
+                  id="name"
+                  className="form-input"
+                  placeholder="Your Name"
+                  required
+                />
+              </div>
+
+              <div className="form-group">
+                <label htmlFor="email" className="form-label">
+                  Email
+                </label>
+                <input
+                  type="email"
+                  name="email"
+                  id="email"
+                  className="form-input"
+                  placeholder="your.email@example.com"
+                  required
+                />
+              </div>
+
+              <div className="form-group">
+                <label htmlFor="message" className="form-label p-josefin">
+                  Message
+                </label>
+                <textarea
+                  name="message"
+                  placeholder="Your Message"
+                  id="message"
+                  className="form-textarea"
+                  required
+                ></textarea>
+              </div>
+
+              <button
+                type="submit"
+                disabled={status === "sending"}
+                className="form-submit"
+              >
+                {status === "sending"
+                  ? "Sending..."
+                  : status === "success"
+                    ? "Sent!"
+                    : "Send Message"}
+              </button>
+
+              {status === "success" && (
+                <p className="form-message form-message-success">
+                  Message sent successfully! 'll get back to you soon.
+                </p>
+              )}
+
+              {status === "error" && (
+                <p className="form-message form-message-error">
+                  Something went wrong. Please try again or email me directly
+                </p>
+              )}
+            </form>
+          </div>
+        </div>
       </div>
     </div>
   );
